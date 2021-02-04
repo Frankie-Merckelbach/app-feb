5a7ff05b026b58259ddd3cb3a1509f13ea3454c8
Frankie Merckelbach <frankiemerckelbach@gmail.com>
2021-02-04 19:04:55 +0100

    made controller files with vies and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # the root file contains the file / page you land on
+  # ROOT = YOUR FILE
+  root to: 'pages#home'
+
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  # Verb.name(action)       to'controller.name#action.name
 end
+
+# controller names are plural always SO pageS, userS
+# model names are always singlar> About, Safe etc
