d70bb8a939b7ee6c07d83f2a62e380fc311a4b15
Frankie Merckelbach <frankiemerckelbach@gmail.com>
2021-02-05 13:03:59 +0100

    css setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+
+# puts "Cleaning database..."
+# User.destroy_all
+
+# puts "Creating Users..."
+# dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+# pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   user = User.create!(attributes)
+#   puts "Created #{user.name}"
+# end
+# puts "Finished!"
