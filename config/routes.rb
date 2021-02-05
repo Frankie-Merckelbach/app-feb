9b5140d0f125e6f7ec9f5c040b844e66826ce2f1
Frankie Merckelbach <frankiemerckelbach@gmail.com>
2021-02-05 12:26:42 +0100

    Rails new with frontend and form gems

--- config/routes.rb
+++ config/routes.rb
@@ -11,3 +11,21 @@ end
 
 # controller names are plural always SO pageS, userS
 # model names are always singlar> About, Safe etc
+
+# to read you write GET
+# to read ONE you write get '/users/:id', to: 'users#show'
+# to create new user you write
+# get '/users/new', to: 'users#new' will display the from
+# THEN you write POST '/users', to: 'users#create'
+
+# to UPDATE write
+# get 'users/:id/edit', to: 'users#edit'
+# then write to finsih updating
+# Patch '/users/:id', to: 'users#update'
+
+# to delete write
+# delete '/users/:id', to: 'users#destroy'
+
+# instead of writing all above
+# you write
+# resources :users
