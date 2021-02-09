9f55f87fbfa256d6af227a48239c3207d3f7fec5
Frankie Merckelbach <frankiemerckelbach@gmail.com>
2021-02-09 14:56:33 +0100

    navbar with ease

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,7 +20,12 @@ import 'bootstrap';
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 
+import { loadDynamicBannerText } from '../components/banner';
+
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
   initUpdateNavbarOnScroll();
+  loadDynamicBannerText();
+
 });
+
